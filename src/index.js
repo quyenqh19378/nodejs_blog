4889f90df12e50d991c7570e14d0bb0121218d87
quyenqh <quyenqh19378@gmail.com>
2023-07-29 08:23:16 +0700

    Remove public/css folder

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
-app.engine("handlebars", handlebars());
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'hbs');
 
-app.set("views", path.join(__dirname, "resources\\views"));
-console.log(path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(path.join(__dirname, 'views'));
 
 // Routes
-app.get("/", function (req, res) {
-    res.render("home");
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/news', function (req, res) {
+    res.render('news');
 });
 
 app.listen(port, () => {
